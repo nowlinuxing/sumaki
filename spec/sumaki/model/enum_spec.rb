8963bee86cff4bf66a63f32da7f8db6d7616b5af
Loose Coupling <loosecpl@gmail.com>
2024-05-17 14:19:12 +0900

    Remove unnecessary tests
    The testing of enums is done in minenum gem, so only minimal testing is performed here.

--- spec/sumaki/model/enum_spec.rb
+++ spec/sumaki/model/enum_spec.rb
@@ -4,45 +4,13 @@ RSpec.describe Sumaki::Model::Enum do
   describe '.enum' do
     class SumakiEnumTest
       include Sumaki::Model
-      enum :int, { a: 1, b: 2, c: 3 }
-      enum :str, { a: 'aaa', b: 'bbb', c: 'ccc' }
-      enum :sym, { a: :a_sym, b: :b_sym, c: :c_sym }
+      enum :type, { a: 1, b: 2, c: 3 }
     end
 
-    [
-      [1, :a],
-      [2, :b],
-      [3, :c]
-    ].each do |actual, expected|
-      context "when the value of int is #{actual}" do
-        subject(:wrapped) { SumakiEnumTest.new({ int: actual }) }
+    describe 'getter' do
+      subject(:wrapped) { SumakiEnumTest.new({ type: 2 }) }
 
-        it { expect(wrapped.int.name).to eq(expected) }
-      end
-    end
-
-    [
-      ['aaa', :a],
-      ['bbb', :b],
-      ['ccc', :c]
-    ].each do |actual, expected|
-      context "when the value of str is #{actual}" do
-        subject(:wrapped) { SumakiEnumTest.new({ str: actual }) }
-
-        it { expect(wrapped.str.name).to eq(expected) }
-      end
-    end
-
-    [
-      ['a_sym', :a],
-      ['b_sym', :b],
-      ['c_sym', :c]
-    ].each do |actual, expected|
-      context "when the value of sym is #{actual}" do
-        subject(:wrapped) { SumakiEnumTest.new({ sym: actual }) }
-
-        it { expect(wrapped.sym.name).to eq(expected) }
-      end
+      it { expect(wrapped.type.name).to eq(:b) }
     end
   end
 end
