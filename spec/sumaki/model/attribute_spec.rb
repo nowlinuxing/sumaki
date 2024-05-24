74b3ef779b0ec2e78b65fc617f3a666af5f85f65
Loose Coupling <loosecpl@gmail.com>
2024-05-24 09:20:58 +0900

    Remove tests for fields of associations

--- spec/sumaki/model/attribute_spec.rb
+++ spec/sumaki/model/attribute_spec.rb
@@ -18,107 +18,31 @@ RSpec.describe Sumaki::Model::Attribute do
   end
 
   describe '.field' do
-    subject(:wrapped) do
-      SumakiAttributeTest.new(
-        {
-          a: 2,
-          foo: {
-            b: 3
-          },
-          bar: [
-            { c: 5 }
-          ]
-        }
-      )
-    end
+    subject(:wrapped) { SumakiAttributeTest.new({ a: 2 }) }
 
     class SumakiAttributeTest
       include Sumaki::Model
-      singular :foo
-      repeated :bar
       field :a
-
-      class Foo
-        include Sumaki::Model
-        field :b
-      end
-
-      class Bar
-        include Sumaki::Model
-        field :c
-      end
     end
 
     it 'defines a method that returns a value' do
       expect(wrapped.a).to eq(2)
     end
 
-    context 'when nested using singular' do
-      it 'defines a method that returns a value' do
-        expect(wrapped.foo.b).to eq(3)
-      end
-    end
-
-    context 'when nested using repeated' do
-      it 'defines a method that returns a value' do
-        expect(wrapped.bar[0].c).to eq(5)
-      end
-    end
-
     context 'when the defined method is overrided' do
-      subject(:wrapped) do
-        SumakiOverrideAttributeTest.new(
-          {
-            a: 2,
-            foo: {
-              b: 3
-            },
-            bar: [
-              { c: 5 }
-            ]
-          }
-        )
-      end
+      subject(:wrapped) { SumakiOverrideAttributeTest.new({ a: 2 }) }
 
       class SumakiOverrideAttributeTest
         include Sumaki::Model
-        singular :foo
-        repeated :bar
         field :a
 
         def a
           super * 7
         end
-
-        class Foo
-          include Sumaki::Model
-          field :b
-
-          def b
-            super * 11
-          end
-        end
-
-        class Bar
-          include Sumaki::Model
-          field :c
-
-          def c
-            super * 13
-          end
-        end
       end
 
       it 'overrides the defined method' do
         expect(wrapped.a).to eq(2 * 7)
-      end
-
-      it 'overrides the defined method in the nested singular class' do
-        expect(wrapped.foo.b).to eq(3 * 11)
-      end
-
-      it 'overrides the defined method in the nested repeated class' do
-        expect(wrapped.bar[0].c).to eq(5 * 13)
       end
     end
   end
